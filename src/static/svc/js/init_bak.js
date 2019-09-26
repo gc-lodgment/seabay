73c5d2040e98d2d42a1c409c4ef4762933d17843
som <d.sssom13@gmail.com>
2019-09-26 09:02:40 +0900

    영상 수정 변경 바꿔봣숩니다

--- src/static/svc/js/init_bak.js
+++ src/static/svc/js/init_bak.js
@@ -2,6 +2,7 @@
 $(function() {
     
     vidWH();
+    vidPC();
     
 	$(window).on('resize', function () {
         vidWH();
@@ -24,15 +25,13 @@ function vidWH(){
     var winWid = $(window).width(),
         winHt = $(window).height(),
         vid = $('.vid-wrap');
-    var txtBoxM = $('.vid-txt-box');
-    var txtBoxWid = txtBoxM.width();
     
     vid.css({'width' : winWid+'px', 'height' : winHt+'px'}); 
     
     if( winWid <= '991' ){
         //console.log('m');
-        
-        $("#vidPc").stop();
+        var txtBoxM = $('.vid-txt-box');
+        var txtBoxWid = txtBoxM.width();
         $('#vidM').get(0).load();
         $('.vid-area.m .vid-box').css({'height' : winHt+'px'}); 
 
@@ -43,7 +42,7 @@ function vidWH(){
             });
         });
         
-        txtBoxM.css({'left' : '50%', 'margin-left' : '-'+(txtBoxWid/2)+'px'});
+        txtBoxM.css({'left' : '50%', 'margin-left' : '-'+(txtBoxWid/2)+'px'})
     }else {
         //var myPlayer;
         $("#vidM").stop();
@@ -56,9 +55,48 @@ function vidWH(){
             vid.find('.vid-txt-box').removeClass('on');
             $('.weather').removeClass('active');
         });
-        
-        txtBoxM.css({'left' : '0', 'margin-left' : 'auto'});
         //vid.find('.vidPc').css({'width' : winWid+'px', 'height' : '100%'}); 
         
     }
+}
+
+function vidPC(){
+    var myPlayer, curTime, allTime, timeStr;
+
+$(window).ready(function(){
+    var isIframe = function () {
+        var a = !1;
+        try {
+            self.location.href != top.location.href && (a = !0)
+        } catch (b) {
+            a = !0
+        }
+        return a
+    };
+
+    var options = {
+        playOnlyIfVisible: true
+    };
+
+    myPlayer = $(".vidPc").YTPlayer({align:"center,left"});
+
+
+    $('#pcVid').on("YTPTime", function(e){
+        var currentTime = e.time;
+
+        curTime = $("#pcVid").YTPGetTime(); 
+        allTime = $("#pcVid").YTPGetTotalTime();
+        timeStr = allTime.split(':'); 
+
+        //console.log('time - ', curTime, '/ alltime - ', allTime, timeStr[1]);
+
+        if( curTime == ('00 : '+ (Number(timeStr[1]) -2)) ){
+            //console.log('true')
+            $('#pcVid').css('opacity', '0');
+            $('.vid-txt-box').addClass('end');
+            $('.weather').addClass('end');
+        }
+
+    });
+});
 }
