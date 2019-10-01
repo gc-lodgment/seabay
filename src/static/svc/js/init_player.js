0e673e3b9b37e7390a6e7df28ab5a34bc8ca8e0b
som <d.sssom13@gmail.com>
2019-10-01 14:06:27 +0900

    영상수정

--- src/static/svc/js/init_player.js
+++ src/static/svc/js/init_player.js
@@ -1,25 +1,38 @@
-$(window).on('load', function () {
-    endVid();
-	$(window).on('resize', function () {
+
+$(function() {
+    $(window).on('load', function () {
         endVid();
-	});
-})
+        $(window).on('resize', function () {
+            endVid();
+        });
+    });
+});
 
 function endVid(){
     //console.log('loaded');
+    var agent = navigator.userAgent.toLowerCase();
+    var winWid = $(window).width();
     
     if( winWid <= '991' ){
     }else {
-        var winWid = $(window).width();
-        var runtime = 17000; //영상 길이
-        var delay = 1000;
-        var time = runtime + delay;
-        setTimeout(function () {
-            //console.log('ended');
-            $('#vidPc').fadeOut();
-            //$('#player').css('opacity', '0');
+        //console.log('pc');
+        if( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) || (agent.indexOf("chrome") != -1) ) {
+            //alert("익스, 크롬");
+            var runtime = 17000; //영상 길이
+            var delay = 1000;
+            var time = runtime + delay;
+            
+            setTimeout(function () {
+                //console.log('ended');
+                $('#vidPc').fadeOut();
+                //$('#player').css('opacity', '0');
+                $('.vid-txt-box').addClass('end');
+                $('.weather').addClass('end');
+            }, time);
+        }else {
+            $("#vidPc").remove(); 
             $('.vid-txt-box').addClass('end');
             $('.weather').addClass('end');
-        }, time);
+        }
     }
 }
