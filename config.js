4e22d8a802db00d52326f295a5f84e5b23bec8f1
vxggangxv <vxggangxv@naver.com>
2019-10-01 14:41:54 +0900

    update;

--- config.js
+++ config.js
@@ -1,78 +1,78 @@
 module.exports = function () {
 
-	var source = 'src',
-		development = 'dist',
-		test = 'test',
-		remove = ['.sass-cache', 'dist'],
+    var source = 'src',
+        development = 'dist',
+        test = 'test',
+        remove = ['.sass-cache', 'dist'],
 
-		// 템플릿 경로
-		template = {
-			src: source + '/template/**/!(_)*.html',
-			parts: source + '/template/**/_*.html',
-			dest: development + '/views',
-			src_m: source + '/template_m/**/!(_)*.html',
-			parts_m: source + '/template_m/**/_*.html',
-			dest_m: development + '/views_m',
-		},
+        // 템플릿 경로
+        template = {
+            src: source + '/template/**/!(_)*.html',
+            parts: source + '/template/**/_*.html',
+            dest: development + '/views',
+            src_m: source + '/template_m/**/!(_)*.html',
+            parts_m: source + '/template_m/**/_*.html',
+            dest_m: development + '/views_m',
+        },
 
-		// Sass 경로
-		sass = {
-			src: source + '/sass/**/!(_)*.{scss,sass}',
-			parts: source + '/sass/**/_*.{scss,sass}',
-			dest: development + '/static'
-		},
+        // Sass 경로
+        sass = {
+            src: source + '/sass/**/!(_)*.{scss,sass}',
+            parts: source + '/sass/**/_*.{scss,sass}',
+            dest: development + '/static'
+        },
 
-		// Css 경로
-		css = {
-			src: source + '/css/**/*.css',
-			dest: development + '/static/css'
-		},
+        // Css 경로
+        css = {
+            src: source + '/**/css/**/*.css',
+            dest: development + '/static/css'
+        },
 
-		// JS 경로
-		js = {
-			src: source + '/js/**/*.js',
-			dest: development + '/static/js'
-		},
+        // JS 경로
+        js = {
+            src: source + '/**/js/**/*.js',
+            dest: development + '/static/js'
+        },
 
-		// Img 경로
-		img = {
-			// src : source + '/assets/img/**/*.{gif,jpg,png,ico}',
-			src: source + '/img/**/!(sprite)*/*',
-			src_sprite: source + '/img/**/sprite*/*',
-			dest: development + '/img',
-		},
+        // Img 경로
+        img = {
+            // src : source + '/assets/img/**/*.{gif,jpg,png,ico}',
+            src: source + '/**/img/**/!(sprite)*/*',
+            src_sprite: source + '/img/**/sprite*/*',
+            dest: development + '/img',
+        },
 
-		// etc 경로
-		etc = {
-			src: source + '/!(css|static|sass|js|img|template)*/**',
-			dest: development + '/static',
-		},
+        // etc 경로
+        etc = {
+            src: source + '/**/!(css|static|sass|js|img|template)*/**',
+            dest: development + '/static',
+        },
 
-		// static (씨베이 별도) 경로
-		static = {
-			src: source + '/static/**',
-			dest: development + '/static',
-		},
-		
-		// HTML 옵션
-		htmlbeautify = {
-			"indentSize": 4
-		};
+        // static (씨베이 별도) 경로
+        static = {
+            src: source + '/static/**',
+            dest: development + '/static',
+        },
 
-	return {
-		del: remove,
-		src: source,
-		test: test,
-		dev: development,
+        // HTML 옵션
+        htmlbeautify = {
+            "indentSize": 4
+        };
 
-		template: template,
-		css: css,
-		sass: sass,
-		js: js,
-		img: img,
-		etc: etc,
-		static: static,
+    return {
+        del: remove,
+        src: source,
+        test: test,
+        dev: development,
 
-		htmlbeautify: htmlbeautify
-	};
+        template: template,
+        css: css,
+        sass: sass,
+        js: js,
+        img: img,
+        etc: etc,
+        static: static,
+
+        htmlbeautify: htmlbeautify
+    };
 };
