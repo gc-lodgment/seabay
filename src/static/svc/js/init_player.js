c5e1c4b7a37b9a31c83576be46f5b5c0ddae21bb
som <d.sssom13@gmail.com>
2019-10-02 18:22:19 +0900

    영상 수정

--- src/static/svc/js/init_player.js
+++ src/static/svc/js/init_player.js
@@ -1,56 +1,54 @@
+var playerState;
+var tag = document.createElement('script');
 
-$(function() {
-    $(window).on('load', function () {
-        endVid();
-        $(window).on('resize', function () {
-            endVid();
-        });
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        //videoId: '-5gWcHBdXWE',
+        /*playerVars : {
+            'controls': 0, //플레이어 컨드롤러 표시여부
+            'rel': 0, //연관동영상 표시여부
+            'playsinline': 1, //ios환경에서 전체화면으로 재생하지 않게하는 옵션
+            'autoplay': 1, //자동재생 여부(모바일에서 작동하지 않습니다. mute설정을 하면 작동합니다.)
+            'loop': 0,
+            //'mute': 1
+        },*/
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
     });
-});
+}
 
-function endVid(){
-    //console.log('loaded');
-    var agent = navigator.userAgent.toLowerCase();
-    var winWid = $(window).width();
-    
-    if( winWid <= '991' ){
-        $('.vid-area.pc .vid-box iframe').remove(); 
-        
-        //$('#vidPc').pauseVideo();
-        $('#vidM').get(0).play();
+function onPlayerReady(event) {
+    player.mute();
+    event.target.playVideo();
+}
 
-        
-    }else {
-        var pchtml = '<iframe id="vidPc" class="vidPc" src="https://www.youtube.com/embed/NupIKt3LM1Q?rel=0&amp;controls=0&amp;autoplay=1&modestbranding=1" allow="autoplay; encrypted-media" frameborder="0" allowfullscreen=""></iframe>';
-        
-        $("#vidM").get(0).pause();
-        $('#vidM').get(0).currentTime = 0;
-        
-        if ( $('.vid-area.pc .vid-box iframe').length > 0 ) {
-            //console.log('있음');
-        } else {
-            //console.log('없음');
-            $('.vid-area.pc .vid-box').append(pchtml);
-        }
-        
-        //console.log('pc');
-        if( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) || (agent.indexOf("chrome") != -1) ) {
-            //alert("익스, 크롬");
-            var runtime = 17000; //영상 길이
-            var delay = 1000;
-            var time = runtime + delay;
-            
-            setTimeout(function () {
-                //console.log('ended');
-                $('#vidPc').fadeOut();
-                //$('#player').css('opacity', '0');
-                $('.vid-txt-box').addClass('end');
-                $('.weather').addClass('end');
-            }, time);
-        }else {
-            $("#vidPc").remove(); 
-            $('.vid-txt-box').addClass('end');
-            $('.weather').addClass('end');
-        }
+function onPlayerStateChange(event) {
+    playerState = event.data == YT.PlayerState.ENDED ? '종료됨' :
+            event.data == YT.PlayerState.PLAYING ? '재생 중' :
+            event.data == YT.PlayerState.PAUSED ? '일시중지 됨' :
+            event.data == YT.PlayerState.BUFFERING ? '버퍼링 중' :
+            event.data == YT.PlayerState.CUED ? '재생준비 완료됨' :
+            event.data == -1 ? '시작되지 않음' : '예외';
+
+    //console.log('onPlayerStateChange 실행: ' + playerState);
+
+    if(playerState == '종료됨'){
+        //console.log('end');
+        $('#player').css('opacity', '0');
+        $('.vid-txt-box').addClass('end');
+        $('.weather').addClass('end');
+    }else if(playerState == '시작되지 않음'){
+        //console.log('yet');
+        player.mute();
+        player.playVideo();
     }
 }
+
---
+++ src/static/svc/js/init_vid.js
@@ -0,0 +1,68 @@
+
+$(document).on('ready', function () {
+    endVid();
+    $(window).on('resize', function () {
+        endVid();
+    });
+});
+
+function endVid(){
+    //console.log('loaded');
+    var agent = navigator.userAgent.toLowerCase();
+    var winWid = $(window).width();
+    
+    if( winWid <= '991' ){
+        var mhtml = '<video id="vidM" playsinline="" webkit-playsinline="" preload="" autoplay="" muted=""><source src="http://img.seabay.co.kr/svc/img/main/seabay_mobile.mp4" type="video/mp4" /></video>';
+        
+        $('.vid-area.pc .vid-box iframe').remove(); 
+        
+        if ( $('.vid-area.m .vid-box video').length > 0 ) {
+            //console.log('있음');
+        }else {
+            //console.log('없음');
+            $('.vid-area.m .vid-box').append(mhtml);
+        }
+
+        
+    }else {
+        var pchtml;
+        
+        $("#vidM").remove(); 
+        $("#vidPc, #player").remove(); 
+        
+        //console.log('pc');
+        if( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {
+            //console.log('IE');
+            pchtml = '<iframe id="vidPc" class="vidPc" src="https://www.youtube.com/embed/NupIKt3LM1Q?rel=0&amp;controls=0&amp;autoplay=1&modestbranding=1" allow="autoplay; encrypted-media" frameborder="0" allowfullscreen=""></iframe>';
+            
+            var runtime = 17000; //영상 길이
+            var delay = 1000;
+            var time = runtime + delay;
+            
+            setTimeout(function () {
+                //console.log('ended');
+                $('#vidPc').fadeOut();
+                //$('#player').css('opacity', '0');
+                $('.vid-txt-box').addClass('end');
+                $('.weather').addClass('end');
+            }, time);
+        }else if ( (agent.indexOf("chrome") != -1) || (agent.indexOf("safari") != -1) || (agent.indexOf("whale") != -1) ){
+            //console.log("SAFARI");
+            if( (agent.indexOf("chrome") != -1) || (agent.indexOf("whale") != -1) ){
+                //console.log('chrome, whale');
+                pchtml = '<iframe id="player" class="vidPc" src="https://www.youtube.com/embed/NupIKt3LM1Q?rel=0&enablejsapi=1&autoplay=1&mute=1" allow="autoplay; encrypted-media" frameborder="0" allowfullscreen=""></iframe>';
+            }else {
+                //console.log('safari');
+                $("#vidPc").remove(); 
+                $('.vid-txt-box').addClass('end');
+                $('.weather').addClass('end');
+            }
+        }else {
+            console.log('other');
+            pchtml = '<iframe id="player" class="vidPc" src="https://www.youtube.com/embed/NupIKt3LM1Q?rel=0&enablejsapi=1&autoplay=1&mute=1" allow="autoplay; encrypted-media" frameborder="0" allowfullscreen=""></iframe>';
+        }
+            
+        $('.vid-area.pc .vid-box').append(pchtml);
+    }
+}
+
