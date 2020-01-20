1b2ab02deb25eecbc29ccf5ea54f813bbcaa65e7
vxggangxv <vxggangxv@naver.com>
2020-01-20 11:18:48 +0900

    edit popup

---
+++ server/www/web-home/restaurant/valueplate.php
@@ -0,0 +1,65 @@
+
+
+    <div class="body"></div>
+    <br class="br_sub">
+
+    <div class="restbar">
+		<?php
+			include("./navi.php");
+		?>
+        <div class="sub container">
+            <div class="sub_item_box">
+                <div class="sub_item_wrap">
+                    <div class="sub_item">
+                        <div class="slick_slider">
+                            <div class="icon-prev"><i class="icon-33"></i></div>
+                            <div class="icon-next"><i class="icon-32"></i></div>
+                            <div class="slick_basic">
+                                <div><img src="http://img.seabay.co.kr/svc/img/facilities/valueplate1cd58.jpg"></div>
+                                <div><img src="http://img.seabay.co.kr/svc/img/facilities/valueplate2cd58.jpg"></div>
+                                <div><img src="http://img.seabay.co.kr/svc/img/facilities/valueplate3cd58.jpg"></div>
+                                <div><img src="http://img.seabay.co.kr/svc/img/facilities/valueplate4cd58.jpg"></div>
+                            </div>
+                        </div>
+                        <div class="item_info">
+                            <div class="info_wrap">
+                                <h5 class="info_title">위치</h5>
+                                <div class="info_content">지하 1층</div>
+                            </div>
+                            <div class="info_wrap">
+                                <h5 class="info_title">운영시간</h5>
+                                <div class="info_content">
+                                    <!-- 07:00 ~ 10:00 (마지막 입장 09:30)
+                                    * 매주 금, 토, 일, 월요일 운영
+                                    * 12월 추가 운영일 : 25일, 31일 -->
+                                    2020년 01월 13일부터 내부 리뉴얼로 이용불가
+                                </div>
+                            </div>
+                            <div class="info_wrap">
+                                <h5 class="info_title">조식요금</h5>
+                                <div class="info_content">
+                                    <!-- 조식요금 (10% 세금 포함)<br />
+                                    * 주중(월-금)성인 : KRW 11,000<br />
+                                    * 주말(토-일&휴일)성인 : KRW 22,000<br />
+                                    * 주중소인 (만 7세 이상 ~ 만 12세 이하) : KRW 7,700<br />
+                                    * 주말소인 (만 7세 이상 ~ 만 12세 이하) : KRW 11,000<br />
+                                    ※ 부모를 동반한 만 7세 미만 (미취학 아동) : 무료 (최대 2명) -->
+									주중/주말 공통<br />
+                                    * 대인 : 22,000원<br />
+                                    * 소인 : 11,000원 (13세 미만)<br />
+                                    ※ 조식 요금은 현장 상황에 의해 변경될 수 있습니다.
+                                </div>
+                            </div>
+                            <div class="info_wrap">
+                                <h5 class="info_title">예약 문의</h5>
+                                <div class="info_content">
+                                    전화 : <span class="sh_small">1666-1243</span><br />
+                                    이메일 : <span class="sh_small">valuehotelgangneung@ebiznetworks.co.kr</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
