7a64e79d88615e8732bf110c674c1c594dc1548f
vxggangxv <vxggangxv@naver.com>
2020-01-29 17:09:23 +0900

    edit service, access

---
+++ server/www/web-home/rooms/description.php
@@ -0,0 +1,83 @@
+<div class="description">
+    <div class="title">
+        <h5>SERVICE & BENEFIT</h5>
+    </div>
+    <div class="cont">
+        <ul class="c_list">
+            <?php if($_GET["r"] == "dx_korea") { ?>
+            <li class="dot-no">※ 한실 이용 안내사항<br>
+            현재 한실의 경우 바닥난방이 불가하여 고객님께는 전기장판이 제공되고있습니다.<br>
+            해당 전기장판은 7-8인용으로 구비되어있으나,<br>
+            1인용침구 4채를 펴시게될 경우 4채의 침구보다 전기장판이 작을수있습니다.<br>
+            객실내 히터는 사용이 가능하오니 객실이용에 참고부탁드립니다.<br></li>
+            <?php } ?>
+            <li>체크인 (입실) 15:00 / 체크아웃 (퇴실) : 11:00</li>
+            <li>[안내사항] 늦은 퇴실 또는 빠른 입실시 다음의 추가요금이 부과됩니다.</li>
+            <li>늦은 퇴실시 부과요금 (레이트 체크아웃)</li>
+            <li class="dot-no">※ 11시~12시 퇴실시 : 무료이용</li>
+            <li class="dot-no">※ 12시~17시 퇴실시 : 시간 상관없이 50,000원</li>
+            <li class="dot-no">※ 17시 이후 퇴실시 : 당일 요금의 100% 부과</li>
+            <li class="dot-no">※ 호텔 상황에 따라 유동적일 수 있음</li>
+            <li>빠른 입실시 부과요금 (얼리 체크인)</li>
+            <li class="dot-no">※ 호텔 상황에 따라 유동적일 수 있음</li>
+            <li>전 객실 금연</li>
+            <li>생수 1인 1병 제공(1박)</li>
+            <li>강릉씨베이호텔 GO GREEN 정책</li>
+            <li class="dot-no">강릉씨베이호텔은 환경보호를 위해 고그린 정책에 동참하고 있습니다.</li>
+            <li class="dot-no">환경보호의 일환으로 연박고객님의 경우, 별도의 객실정비를 진행하지 않습니다.</li>
+            <li class="dot-no">필요한 물품(수건, 생수 등)은 프론트에서 교체 제공해 드리고 있으니 이용에 참고하시기 바랍니다.</li>
+        </ul>
+    </div>
+</div>
+<div class="description">
+    <div class="title">
+        <h5>RESERVATION & CANCELLATION</h5>
+    </div>
+    <div class="cont">
+        <ul class="c_list">
+            <li>
+                예약문의: 1666-1243
+            </li>
+            <li>
+                E-mail: valuehotelgangneung@ebiznetworks.co.kr
+            </li>
+            <li>
+                취소/환불 규정<br>* 비수기/연휴/성수기 공통 <br>
+                <table class="detail_table">
+                    <tr>
+                        <td>체크인 15일 이전 취소</td>
+                        <td class="font-red">90%환불</td>
+                    </tr>
+                    <tr>
+                        <td>체크인 10일 이전 취소</td>
+                        <td class="font-red">80%환불</td>
+                    </tr>
+                    <tr>
+                        <td>체크인 7일 이전 취소</td>
+                        <td class="font-red">60%환불</td>
+                    </tr>
+                    <tr>
+                        <td>체크인 6일 이전 취소</td>
+                        <td class="font-red">30%환불</td>
+                    </tr>
+                    <tr>
+                        <td>체크인 5일전~당일취소<span class="font-red">(No Show)</span></td>
+                        <td class="font-red">환불불가</td>
+                    </tr>
+                </table>
+                * 체크인 시간 기준 : 당일 15시 입실 기준<br>
+            </li>
+        </ul>
+    </div>
+</div>
+<!--div class="policies">
+    <div class="title">
+        <h5>HOW TO GET TO HERE</h5>
+    </div>
+    <div class="cont">
+        <div class="btn_box">
+            <a class="b_btn_darkgray" href="../../main/map/index.html">상세 정보 확인하기</a>
+        </div>
+    </div>
+</div-->
+<!-- /.policies -->
